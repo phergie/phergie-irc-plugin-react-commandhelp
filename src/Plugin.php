e7ea25329a46f2ccd45b79023a6a3a0b61e107d2
Joe Ferguson <joe@joeferguson.me>
2016-04-25 18:26:23 -0500

    update package docblock

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,7 +5,7 @@
  * @link https://github.com/phergie/phergie-irc-plugin-react-commandhelp for the canonical source repository
  * @copyright Copyright (c) 2008-2014 Phergie Development Team (http://phergie.org)
  * @license http://phergie.org/license Simplified BSD License
- * @package Phergie\Irc\Plugin\React\CommandHelp
+ * @package phergie/phergie-irc-plugin-react-commandhelp
  */
 
 namespace Phergie\Irc\Plugin\React\CommandHelp;
